1283b4894529d7188c42ebd858b64335971b5f14
Qodirxon2000aa <namesuz41@gmail.com>
2026-02-03 19:03:47 +0500

    sas

--- src/app/pages/ChekPage.jsx
+++ src/app/pages/ChekPage.jsx
@@ -3,17 +3,17 @@ export default function ChekPage() {
     <div className="p-4">
       <div className="rounded-xl border border-dashed border-orange-400 bg-orange-50 p-4">
         <h2 className="text-lg font-semibold text-orange-600">
-          🧪 TEST CHEK
+          quuuuuuuuuuuuuuuuuuuv
         </h2>
 
         <p className="mt-2 text-sm text-gray-700">
-          Bu sahifa <b>startapp=chek</b> orqali ochildi.
+           <b>chek</b> 
         </p>
 
         <div className="mt-4 rounded-lg bg-white p-3 shadow-sm">
           <div className="flex justify-between text-sm">
             <span>Holat:</span>
-            <span className="font-medium text-green-600">FAKE / TEST</span>
+            <span className="font-medium text-green-600"></span>
           </div>
 
           <div className="flex justify-between text-sm mt-1">
