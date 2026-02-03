ef066c80311a696b8fb1ca7583551bee24f3a2ec
Qodirxon2000aa <namesuz41@gmail.com>
2026-02-03 19:06:18 +0500

    sa

--- src/app/layouts/RootLayout.tsx
+++ src/app/layouts/RootLayout.tsx
@@ -1,15 +1,22 @@
-import { Outlet } from 'react-router';
+import { Outlet, useLocation } from 'react-router';
 import { BottomNav } from '@/app/components/BottomNav';
 import ChekListener from "@/app/pages/ChekListener";
 
 export function RootLayout() {
+  const location = useLocation();
+
+  // 🔥 faqat shu pageda footer yo‘q
+  const hideBottomNav = location.pathname === "/chek";
+
   return (
     <div className="min-h-screen bg-background pb-16">
       <div className="max-w-md mx-auto">
-       <ChekListener />
+        <ChekListener />
         <Outlet />
       </div>
-      <BottomNav />
+
+      {/* 🔻 footer shart bilan */}
+      {!hideBottomNav && <BottomNav />}
     </div>
   );
 }
